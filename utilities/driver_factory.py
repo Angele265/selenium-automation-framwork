3dbf6c9014ae96f1bbd839835319dd5c3c04d807
Angele <angelevelda@yahoo.com>
2026-07-10 10:51:34 +0200

    create selenium driver factory

---
+++ utilities/driver_factory.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+class DriverFactory:
+
+    @staticmethod
+    def get_chrome_driver():
+
+        options = Options()
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-popup-blocking")
+
+        driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+
+        driver.implicitly_wait(10)
+
+        return driver
